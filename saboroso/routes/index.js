505551289c141ed14319fba630cc493a2fa3ef0e
JUNIOR_OKOSTA <82908127+JUNIOROKOSTA@users.noreply.github.com>
2022-12-27 15:50:43 -0300

    init v1
    Começando os trabalhos. Vivendo e aprendendo.

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -1,9 +1,25 @@
+const conn = require('./../inc/db')
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  conn.query(
+    `
+    SELECT * FROM tb_menus ORDER BY title
+    `, (err,results)=>{
+      if(err){
+        console.log('ERROR:> ' , err);
+      }
+
+      res.render('index', {
+        title: 'Restaurant Saboroso!',
+        menus: results
+      })
+
+    });
+
 });
 
 module.exports = router;
